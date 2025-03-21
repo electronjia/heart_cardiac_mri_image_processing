97b915df669615bc15ecef892ca47b7cc447fa3c
Mary K <mkkadyrowa@gmail.com>
2025-03-21 12:44:42 -0400

    Delete manualcontours folder, upload code for getting mask filepaths in excel

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+user_handle = r"C:\Users\mkkad\Desktop\github_repos\heart_cardiac_mri_image_processing\data"
+
+patient_data_excel_path = r"C:\Users\mkkad\Desktop\github_repos\heart_cardiac_mri_image_processing\scd_patientdata_xlsx.xlsx"
+
+patient_data = "patient_data"
+patient_filepaths = "patient_filepaths"
