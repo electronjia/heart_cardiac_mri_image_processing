4c746b8b4cffb33977840d311ac9c6b36b1d8e65
Mary K <mkkadyrowa@gmail.com>
2025-03-22 18:56:07 -0400

    Edge detection code for all patients

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ patient_data_excel_path = r"C:\Users\mkkad\Desktop\github_repos\heart_cardiac_mr
 
 patient_data = "patient_data"
 patient_filepaths = "patient_filepaths"
+
+
+# Mahri's identifier for LV segmentation mask results 
+mahri_identifier = "_mask_edge_detection.png"
+kaiwen_identifier = "_mask_marching_cubes.png"
+shimin_identifier = "_mask_watershed.png"
